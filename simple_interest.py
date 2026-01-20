4df3949ab02853514b2d1297d28ca7e6b3346a67
Jean Julmis <jeanjulmis87@gmail.com>
2026-01-20 15:03:29 -0600

    Refine comments and input prompts in simple_interest.py
    Updated comments and input prompts for clarity.

--- simple_interest.py
+++ simple_interest.py
@@ -1,10 +1,10 @@
-# This program ask user input and calculate interest on money invest
+# This program asks for user input and calculates interest on invested money
 def earning_interest():
     principal = float(input("Enter the principal: "))
-    interest_rate = float(input("Enter the interest rate "))
+    interest_rate = float(input("Enter the interest rate (%): "))
     time = int(input("Enter the amount of time in years: "))
 
     interest_earned = principal * interest_rate * time / 100 
     print(f"Interest earned: ${interest_earned:.2f}") 
 
-earning_interest() # Call to function
+earning_interest() 
