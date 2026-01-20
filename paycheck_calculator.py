d53f8247b65303da6b0147435a9c2b562b2d6847
Jean Julmis <jeanjulmis87@gmail.com>
2026-01-20 15:10:58 -0600

    Update paycheck calculator to compute weekly pay

--- paycheck_calculator.py
+++ paycheck_calculator.py
@@ -1,9 +1,9 @@
-# This program ask for user input and compute hourly work and rate
+# This program asks for user input and computes weekly pay
 def my_paycheck():
     hours_worked = float(input("Enter hours worked: "))
     hourly_rate = float(input("Enter hourly rate: "))
     
-    total_pay = hours_worked * hourly_rate
-    print(f"I work ${total_pay:.2f} this week.") # Output
+    weekly_pay = hours_worked * hourly_rate
+    print(f"I work ${weekly_pay:.2f} this week.") # Output
 
-my_paycheck() # call to function
+my_paycheck() 
