715d82ab92eb2a4a550365c98be468f1c7e1cd38
Jean Julmis <jeanjulmis87@gmail.com>
2026-01-20 15:37:16 -0600

    Fix output message for Celsius to Fahrenheit conversion

--- convert_celsius_to_fahrenheit.py
+++ convert_celsius_to_fahrenheit.py
@@ -3,6 +3,7 @@ def celsius_to_fahrenheit():
     celsius = float(input("Enter the celsius temperature: "))
     fahrenheit = celsius * 9 / 5 + 32 
 
-    print(f"{celsius} degrees Fahrenheit is {fahrenheit:.1f} degrees Celsius") 
+    print(f"{celsius} degrees Celsius is {fahrenheit:.1f} degrees Celsius") 
 
-celsius_to_fahrenheit() # call to function
+
+celsius_to_fahrenheit() 
