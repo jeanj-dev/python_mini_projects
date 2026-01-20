413934b18b6c3b59b93d1398afc85fa49df680c2
Jean Julmis <jeanjulmis87@gmail.com>
2026-01-20 15:19:20 -0600

    Refactor min_to_hr function for clarity

--- minutes_to_hours.py
+++ minutes_to_hours.py
@@ -1,10 +1,11 @@
-
 # This program converts minutes to hours and remaining minutes
 def min_to_hr():
     minutes = int(input("Enter the number of minutes: "))
-    hours = minutes // 60 # // integer divsion to whole numbers
-    remaining_minutes = minutes % 60 # % get remainder
+    
+    hours = minutes // 60 
+    remaining_minutes = minutes % 60 
     
     print(f"{minutes} minutes = {hours} hours and {remaining_minutes} minutes")
 
 min_to_hr()
+
