09713f71be3b643a3ab602ec89fd579cf6b2d116
Jean Julmis <jeanjulmis87@gmail.com>
2026-06-28 18:06:14 -0500

    Refactor celsius to fahrenheit converter into functions

--- convert_celsius_to_fahrenheit.py
+++ convert_celsius_to_fahrenheit.py
@@ -1,9 +1,21 @@
-# This program ask for user input and convert Celsius to Fahrenheit
-def celsius_to_fahrenheit():
-    celsius = float(input("Enter the celsius temperature: "))
-    fahrenheit = celsius * 9 / 5 + 32 
+# This program asks for user input and converts Celsius to Fahrenheit
 
-    print(f"{celsius} degrees Celsius is {fahrenheit:.1f} degrees Celsius") 
+def celsius_to_fahrenheit(celsius):
+    """Convert a celsius temperature to fahrenheit."""
+    return celsius * 9 / 5 + 32
 
 
-celsius_to_fahrenheit() 
+def celsius_value():
+    """Get user input, call the conversion function, and display the value."""
+    while True:
+        try:
+            celsius = float(input("Enter the celsius temperature: "))
+            break
+        except ValueError:
+            print("Wrong input! Numbers only!")
+
+    fahrenheit = celsius_to_fahrenheit(celsius)
+    print(f"{celsius} degrees Celsius is {fahrenheit:.1f} degrees Fahrenheit")
+
+
+celsius_value()
